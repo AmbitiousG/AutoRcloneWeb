5c31146233325c22e95bf7e0d6151edfef1cafd4
gy <ambitious.gyang@gmail.com>
2019-10-16 21:18:40 +0800

    init nuxtjs config

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <el-button @click="check">Test</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  methods: {
+    check() {
+
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
