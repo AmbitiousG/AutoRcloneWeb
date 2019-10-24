ded2b927f916dee5381833dcb0280b8525280920
gy <ambitious.gyang@gmail.com>
2019-10-24 23:37:31 +0800

    test api

--- components/dashboard/main.vue
+++ components/dashboard/main.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>dashboard</div>
+  <div>
+    <div>dashboard</div>
+    <div class="test">
+      <el-input v-model="command"></el-input>
+      <el-button @click="checkPostEvt">check post</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "dashboard"
-}
+  name: "dashboard",
+  data() {
+    return {
+      command: ""
+    };
+  },
+  methods: {
+    checkPostEvt() {
+      this.$api.post(this.command);
+    }
+  }
+};
 </script>
 
 <style>
-
+.test {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+.test .el-input {
+  margin-right: 10px;
+}
 </style>
--- utils/api.js
+++ utils/api.js
@@ -1,13 +1,26 @@
 export default {
   baseUrl: `${location.href}rc_api/`,
-  async post(path) {
+  async post(path, data) {
+    let body = null;
+    if(!data) {
+      body = JSON.stringify(data);
+    }
     const res = await fetch(this.baseUrl + path, {
       method: "POST", // or 'PUT
+      body
     });
     res.json();
     return res;
   },
+  async asyncPost(path, data) {
+    data = data || {};
+    data = {...data, _async: true};
+    return await this.post(path, data);
+  },
   async getStats() {
     return await this.post("core/stats");
+  },
+  async copy(source, dest) {
+    return await this.asyncPost("sync/copy");
   }
 }
